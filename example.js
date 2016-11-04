ce3ff481b74dba8d29cfc044066a1782ea6ccc6d
shravyagoli <shravyagoli2611@gmail.com>
2016-11-03 23:52:05 -0400

    Add files via upload

--- example.js
+++ example.js
@@ -1,45 +1,140 @@
-// ADD NEW ITEM TO END OF LIST
-// element <ul>
-var list = document.getElementsByTagName('ul')[0];
-//create a new element
-var newItemLast = document.createElement('li');
-// create a TextNode
-var newTextLast = document.createTextNode('cream');
-// add this TextNode to the element
-newItemLast.appendChild(newTextLast);
-//add element at the end
-list.appendChild(newItemLast);
 
-// ADD NEW ITEM START OF LIST
-//create the element
-var newItemFirst = document.createElement('li');
-//create the textnode
-var newTextFirst = document.createTextNode('kale');
-//add the node to element
-newItemFirst.appendChild(newTextFirst);
-// add elemt to the list
-list.insertBefore(newItemFirst, list.firstChild);
-
-// ADD A CLASS OF COOL TO ALL LIST ITEMS
-// here are <li> elements
-var listItems = document.querySelectorAll('li');
-// declare counter variable
-var i;
-// for loop for the elements
-for(i=0; i < listItems.length; i++)
-    {
-        // class name changed to cool
-        listItems[i].className = 'cool';
-    }
-
-// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
-// goes to h2 element
-var heading = document.querySelector('h2');
-// h2 text
-var headingText = heading.firstChild.nodeValue;
-// list elements
-var totalItems = listItems.length;
-// total content 
-var newHeading = headingText + '<span>' + totalItems + '</span>';
-// h2 will be updated by innerHTML
-heading.innerHTML=newHeading;
+	function inputIsValid(cssSelector)
+	{
+	return ($(cssSelector).val() !== "" && !isNaN($(cssSelector).val()));
+	}
+	
+	// For the input 
+	function validateInput(cssSelector)
+	{
+	if (inputIsValid(cssSelector) == false)
+	{
+	$(cssSelector).css("background-color","red");
+	return false;
+	}
+	else
+	{
+	$(cssSelector).css("background-color","white");
+	return true;
+	}
+	}
+	// if inputs are valid then it is true
+	
+	function allInputsAreValid()
+	{
+	var startX = $("#HorizonalStartTextBox").val();
+	var endX = $("#HorizonalEndTextBox").val();
+	var startY = $("#VerticalStartTextBox").val();
+	var endY = $("#VerticalEndTextBox").val();
+	
+	return (inputIsValid("#HorizonalStartTextBox") &&
+	inputIsValid("#HorizonalEndTextBox") &&
+	inputIsValid("#VerticalStartTextBox") &&
+	inputIsValid("#VerticalEndTextBox") &&
+	Number(endX) >= Number(startX) &&
+	Number(endY) >= Number(startY))
+	}
+	
+	// Creates a table based on the inputs and returns the HTML as a string.
+	function generateTable(startX, endX, startY, endY)
+	{
+	var table = "<table class='darkShadedTable'>";
+	
+	startX = Number(startX);
+	startY = Number(startY);
+	endX = Number(endX);
+	endY = Number(endY);
+	
+	// Make the header
+	
+	table += "<tr><th>&nbsp;</th>";
+	for (var x = startX; x <= endX; x++)
+	{
+	table += "<th>" + x + "</th>"
+	}
+	table += "</tr>";
+	
+	// Now do each individual row.
+	for (var y = startY; y <= endY; y++)
+	{
+	table += "<tr><th>" + y + "</th>";
+	
+	for (var x = startX; x <= endX; x++)
+	{
+	table += "<td>" + (x * y) + "</td>";
+	}
+	
+	table += "</tr>";
+	}
+	
+	table += "</table>";
+	
+	return table;
+	}
+	
+	// Checks if all the inputs are valid and, if so, generates the table.
+	function checkAndGenerateTable()
+	{
+	
+	var startX = $("#HorizonalStartTextBox").val();
+	var endX = $("#HorizonalEndTextBox").val();
+	var startY = $("#VerticalStartTextBox").val();
+	var endY = $("#VerticalEndTextBox").val();
+	
+	
+	if (allInputsAreValid())
+	{
+	$("#content").html(generateTable(startX, endX, startY, endY));
+	$("#content").css("display","block");
+	}
+	}
+	
+	jQuery(document).ready(function()
+	{
+	
+	// Whenever the input is changed, let the user know if their input is valid or not immediately.
+	$(".LimitsTextBox").on('input', function()
+	{
+	validateInput("#HorizonalStartTextBox");
+	validateInput("#HorizonalEndTextBox");
+	validateInput("#VerticalStartTextBox");
+	validateInput("#VerticalEndTextBox");
+	
+	
+	checkAndGenerateTable();
+	
+	});
+	
+	$(".LimitsTextBox").blur(function()
+	{
+	// When the user moves stops inputting text into a text box, force the numbers to be "in order"
+	// In other words, make sure the second valid is greater than the first for both groups.
+	// Finally, after doing that, if all input is valid, then generate the table.
+	
+	var startX = $("#HorizonalStartTextBox").val();
+	var endX = $("#HorizonalEndTextBox").val();
+	var startY = $("#VerticalStartTextBox").val();
+	var endY = $("#VerticalEndTextBox").val();
+	
+	// If both horizontal selectors are valid numbers, make sure the second is greater than the first.
+	if (inputIsValid("#HorizonalStartTextBox") && inputIsValid("#HorizonalEndTextBox"))
+	if (Number(endX) < Number(startX))
+	$("#HorizonalEndTextBox").val(Number(startX));
+	
+	// Same for the vertical selectors
+	if (inputIsValid("#VerticalStartTextBox") && inputIsValid("#VerticalEndTextBox"))
+	if (Number(endY) < Number(startY))
+	$("#VerticalEndTextBox").val(Number(startY));
+	
+	
+	checkAndGenerateTable();
+	
+	});
+	
+	
+	
+	checkAndGenerateTable();
+	
+	});
+		
+	
